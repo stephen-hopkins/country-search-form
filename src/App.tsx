fee5dbfa57244912999cb8e2eccbacbb80f12b64
Stephen Hopkins <github@stephenhopkins.dev>
2023-01-20 14:30:24 +0200

    feat: country select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "primeflex/primeflex.min.css";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CountrySelect from "./Components/CountrySelect";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <CountrySelect />
+      </main>
     </div>
   );
 }
---
+++ src/Components/CountrySelect.tsx
@@ -0,0 +1,77 @@
+import React, {useMemo, useRef, useState} from 'react';
+import {MultiSelect} from "primereact/multiselect";
+import {countryList} from "../Helpers/CountryList";
+import {InputText} from "primereact/inputtext";
+import {InputSwitch} from "primereact/inputswitch";
+import './CountrySelect.css';
+import {Button} from "primereact/button";
+
+const countryTemplate = (option: string) => {
+  return (
+    <div className="country-item font-face-cb">
+      <div>{option}</div>
+    </div>
+  );
+}
+
+function CountrySelect() {
+  const [selectedCountries, setSelectedCountries] = useState([] as string[]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showSelectedOnly, setShowSelectedOnly] = useState(false);
+  const multiSelectRef = useRef(null as MultiSelect | null);
+
+  const countries = useMemo(() => {
+    if (showSelectedOnly) {
+      return selectedCountries;
+    }
+    if (searchTerm !== '') {
+      return countryList.filter(c => c.includes(searchTerm));
+    }
+    return countryList;
+  }, [showSelectedOnly, searchTerm, selectedCountries]);
+
+  const clearAll = () => {
+    setSelectedCountries([]);
+    setSearchTerm('');
+    setShowSelectedOnly(false);
+  }
+
+  const closeSelect = () => {
+    multiSelectRef.current?.hide();
+  }
+
+  // unfortunately prime replaces the filter box with the header if it's provided so as well
+  // as the show selected we also need to implement the filter functionality
+  const header = () => {
+    return (
+      <div className="py-2 px-3 font-face-cb">
+        <InputText className="w-full input-search" placeholder="Search" value={searchTerm}
+                   onChange={e => setSearchTerm(e.target.value)}/>
+        <div className="font-bold flex align-items-center mt-3">
+          <InputSwitch className="" checked={showSelectedOnly} onChange={e => setShowSelectedOnly(e.value)}/>
+          <label className="ml-2 mr-6">Show selected only</label>
+          <span className="ml-auto" onClick={clearAll}>Clear all</span>
+        </div>
+      </div>
+    );
+  }
+
+  const footer = () => {
+    return (
+      <div className="py-2 px-3">
+        <div className="cs-footer flex">
+          <Button className="save-button ml-auto mt-2 mb-2" onClick={closeSelect}>Save</Button>
+        </div>
+      </div>
+    )
+  };
+
+  return (
+    <MultiSelect options={countries} itemTemplate={countryTemplate} value={selectedCountries}
+                 onChange={(e) => setSelectedCountries(e.value)} filter={true} showSelectAll={false}
+                 panelHeaderTemplate={header} panelFooterTemplate={footer} className="country-select mt-6"
+                 showClear={true} panelClassName="country-select" ref={multiSelectRef} />
+  );
+}
+
+export default CountrySelect;
